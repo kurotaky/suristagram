c0062c267c4fb3d8587527d496631547fe1ca0c0
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2018-04-05 14:28:31 +0900

    aboutページを追加

---
+++ pages/about/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        About
+      </h1>
+      <div>
+          このサービスはInstagram画像を投稿してグッズを作ることができるサービスです！
+      </div>
+    </div>
+  </section>
+</template>
